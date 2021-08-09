8efcabb0ce501608787e343feaf1577b29471f21
ez0623 <edwardzhang623@gmail.com>
2021-08-09 00:48:14 -0700

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+// components
+import Cat from "./components/Cat";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Cat />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
